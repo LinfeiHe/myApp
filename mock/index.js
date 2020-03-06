8dee84e73e4ba39f1fc8eaabe1e5fe59dabaa76f
helinfei <842789227@qq.com>
2020-03-06 08:32:19 +0800

    修复一些bug

--- mock/index.js
+++ mock/index.js
@@ -16,12 +16,12 @@ const data= {
         )
     },
     //可以直接使用mockjs生成mock数据
-    'GET /api/mock':{
+    'GET /api/mock':mockjs.mock(
         [{title: '我要优惠', description: '建行龙支付'},
         {title: '我要贷款', description: '随借随还，方便快捷'},
         {title: '信用卡申请', description: '一卡在手，方便所有'},
         {title: '财富体验', description: '财富诊断，合理规划'}]
-    }
+    )
 }
 //使用delay方法可以延迟返回数据
 module.exports=delay(data,1000);
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -13,7 +13,7 @@ export default class Index extends Component {
   constructor(props){
     super(props);
     this.state = {
-      seeMore: false
+      seeMore: false,
       gridSet: []
     }
   }
